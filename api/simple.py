9727235d512582cfcee8c07bb1318158c4b7f594
kedistkid <kedistkid723@gmail.com>
2024-07-13 23:48:09 +0300

    remove comment

--- api/simple.py
+++ api/simple.py
@@ -45,7 +45,7 @@ def read_file_as_image(data) -> np.ndarray:
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     image = read_file_as_image(await file.read())
-    img_batch = np.expand_dims(image, 0)  # Add batch dimension
+    img_batch = np.expand_dims(image, 0)
 
     predictions = MODEL.predict(img_batch)
 
